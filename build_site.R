5d1082e8b4b8c3bb3c7a84c3f59f4c24a17a817c
root <root@MANGIN-PC.INTERNAL.COMLINKDATA.COM>
2017-11-19 14:48:02 -0500

    updates

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/Mangin/site")
+setwd("C:/Users/tmangin/site")
 
 #render your sweet site. 
 rmarkdown::render_site()
